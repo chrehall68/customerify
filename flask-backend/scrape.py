61b66d2e23ca48d3384dc20ad093e319a1855143
chrehall68 <chrehall68@gmail.com>
2024-06-23 11:33:02 -0700

    add js-rendered page support

--- flask-backend/scrape.py
+++ flask-backend/scrape.py
@@ -4,6 +4,10 @@ from asyncio.queues import Queue
 import asyncio
 from typing import Callable, Awaitable
 import uuid
+import requests
+import selenium
+import selenium.webdriver as webdriver
+from selenium.webdriver import ChromeOptions
 
 
 def get_domain_name(url: str) -> str:
@@ -11,7 +15,7 @@ def get_domain_name(url: str) -> str:
     last_dot = url.rindex(".")
     domain_start = url.index("//") + 2
     if "." in url[domain_start:last_dot]:
-        domain_start = url[domain_start:last_dot].rindex(".") + 1
+        domain_start = url[:last_dot].rindex(".", domain_start) + 1
 
     domain_end = len(url)
     if "/" in url[last_dot:]:
@@ -45,20 +49,70 @@ async def main(
     if max_urls == -1:
         max_urls = float("inf")
     domain = get_domain_name(base_url)
-    print(domain)
+    print("domain", domain)
     explored_urls = set()
     exploring_urls = set()
     queue = Queue()
 
-    async with aiohttp.ClientSession() as session:
-        # basically, we can do a bfs from the base url
-        await queue.put(base_url)
+    use_selenium = False
+    try:
+        soup = bs4.BeautifulSoup(requests.get(base_url, timeout=2).text, "html.parser")
+        if "You need to enable JavaScript to run this app." in soup.text:
+            use_selenium = True
+    except Exception as e:
+        print(e)
+        return
 
-        async def helper(url: str):
+    if not use_selenium:
+        async with aiohttp.ClientSession() as session:
+            # basically, we can do a bfs from the base url
+            await queue.put(base_url)
+
+            async def helper(url: str):
+                explored_urls.add(url)
+                soup = bs4.BeautifulSoup(
+                    await (await session.get(url, timeout=timeout)).text(),
+                    "html.parser",
+                )
+                for link in soup.find_all("a"):
+                    href = link.get("href", "")
+                    if not is_valid_url(href) and href.startswith("/"):
+                        href = base_url + href
+                    if (
+                        is_valid_url(href)
+                        and href not in explored_urls
+                        and href not in exploring_urls
+                        and is_same_domain(href, domain)
+                    ):
+                        await queue.put(href)
+                        exploring_urls.add(href)
+
+                if document_callback:
+                    await document_callback(soup, str(uuid.uuid4()))
+
+            while queue.qsize() > 0 and len(explored_urls) < max_urls:
+                # take all the urls from the queue
+                urls = []
+                for _ in range(queue.qsize()):
+                    urls.append(await queue.get())
+                # filter urls
+                urls = list(filter(lambda url: url not in explored_urls, urls))
+                urls = list(set(urls))  # take only unique urls
+                await asyncio.gather(*[helper(url) for url in urls])
+    else:
+        print("need to use selenium")
+        options = ChromeOptions()
+        options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
+        await queue.put(base_url)
+        while queue.qsize() > 0 and len(explored_urls) < max_urls:
+            # take next item
+            url = await queue.get()
             explored_urls.add(url)
-            soup = bs4.BeautifulSoup(
-                await (await session.get(url, timeout=timeout)).text(), "html.parser"
-            )
+            driver.get(url)
+            html = driver.execute_script("return document.documentElement.innerHTML;")
+            # print(html)
+            soup = bs4.BeautifulSoup(html, "html.parser")
             for link in soup.find_all("a"):
                 href = link.get("href", "")
                 if not is_valid_url(href) and href.startswith("/"):
@@ -75,16 +129,6 @@ async def main(
             if document_callback:
                 await document_callback(soup, str(uuid.uuid4()))
 
-        while queue.qsize() > 0 and len(explored_urls) < max_urls:
-            # take all the urls from the queue
-            urls = []
-            for _ in range(queue.qsize()):
-                urls.append(await queue.get())
-            # filter urls
-            urls = list(filter(lambda url: url not in explored_urls, urls))
-            urls = list(set(urls))  # take only unique urls
-            await asyncio.gather(*[helper(url) for url in urls])
-
     return explored_urls
 
 
@@ -92,7 +136,7 @@ if __name__ == "__main__":
     import datetime
 
     start = datetime.datetime.now()
-    results = asyncio.run(main(base_url="https://rev.ai/"))
+    results = asyncio.run(main(base_url="https://live.calhacks.io"))
     end = datetime.datetime.now()
     print((end - start).total_seconds())
     print(results)
