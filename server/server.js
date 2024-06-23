cfbf8de67ff4e0dcb5bb8dba619ce37be4f084b3
chrehall68 <chrehall68@gmail.com>
2024-06-22 21:16:29 -0700

    get initial stt with deepgram

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ import bodyParser from "body-parser";
 import Speech from "lmnt-node";
 import VoiceResponse from "twilio/lib/twiml/VoiceResponse.js";
 import { v4 as uuidv4 } from "uuid";
-import fs from 'fs'
+import fs, { writeFileSync } from "fs";
+import { createClient, LiveTranscriptionEvents } from "@deepgram/sdk";
+
+
+const deepgramClient = createClient(process.env.DEEPGRAM_API_KEY);
+var totalBytes = Buffer.alloc(0)
 
 const a = expressWs(app);
 
@@ -31,7 +36,12 @@ app.post("/answer", (req, res) => {
   console.log("post route", req.testing);
   var r = new VoiceResponse();
   r.say("Please say something");
-  r.gather({ input: "speech", action: "/respond", speechTimeout: 1 });
+  const start = r.start();
+  start.stream({
+    name: "testStream",
+    url: "wss://116c-2607-f140-6000-8006-593f-a7d5-8b98-a55e.ngrok-free.app/",
+  });
+  r.pause({ length: 2 });
   res.send(r.toString());
 });
 
@@ -58,10 +68,7 @@ app.post("/respond", async (req, res) => {
   console.log("finished await");
 
   const filename = uuidv4() + ".wav";
-  fs.writeFileSync("./public/" + filename, output.audio, (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
+  fs.writeFileSync("./public/" + filename, output.audio);
 
   r.play(filename);
   r.redirect("/answer");
@@ -70,10 +77,60 @@ app.post("/respond", async (req, res) => {
 });
 
 app.ws("/", function (ws, req) {
-  ws.on("message", function (msg) {
-    console.log(msg);
+  console.log("ws: client connected");
+  const deepgram = deepgramClient.listen.live({
+    smart_format: true,
+    model: "nova-2",
+    encoding:"mulaw",
+    sample_rate:8000,
+    channels: 1,
   });
-  console.log("socket", req.testing);
+
+  deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
+    console.log("deepgram: connected");
+
+    deepgram.addListener(LiveTranscriptionEvents.Transcript, (data) => {
+      console.log("deepgram: transcript received");
+      console.log("ws: transcript sent to client");
+      console.log(JSON.stringify(data));
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
+      console.log("deepgram: disconnected");
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
+      console.log("deepgram: error received");
+      console.error(error);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
+      console.log("deepgram: warning received");
+      console.warn(warning);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
+      console.log("deepgram: metadata received");
+      console.log("ws: metadata sent to client");
+      console.log(JSON.stringify({ metadata: data }));
+    });
+
+    ws.on("message", (message) => {
+      message = JSON.parse(message);
+      console.log("ws: client data received");
+      if (message['event'] === "media"){
+        console.log("ws: data sent to deepgram");
+        totalBytes = Buffer.concat([totalBytes, Buffer.from(message["media"]["payload"], "base64")]);
+        deepgram.send(Buffer.from(message["media"]["payload"], "base64"));
+      }
+    });
+    ws.on("close", () => {
+      console.log("ws: client disconnected");
+      //deepgram.finish();
+      //console.log("finished removing listeners");
+      writeFileSync("totalBytes.txt", totalBytes);
+    });
+  });
 });
 
 app.listen(PORT);
